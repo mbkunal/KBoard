26119dd2a48871a4729b25c4160a91916f28233e
mbkunal <mb.kunal@gmail.com>
2018-08-26 03:46:53 +0530

    c1

--- KBoard/Form1.Designer.cs
+++ KBoard/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Submit = new System.Windows.Forms.Button();
+            this.InBx = new System.Windows.Forms.TextBox();
+            this.Dwell_time = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(155, 331);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(200, 36);
+            this.Submit.TabIndex = 0;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // InBx
+            // 
+            this.InBx.Location = new System.Drawing.Point(9, 243);
+            this.InBx.Name = "InBx";
+            this.InBx.Size = new System.Drawing.Size(498, 22);
+            this.InBx.TabIndex = 1;
+            this.InBx.KeyDown += new System.Windows.Forms.KeyEventHandler(this.InBx_KeyDown);
+            this.InBx.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InBx_KeyPress);
+            this.InBx.KeyUp += new System.Windows.Forms.KeyEventHandler(this.InBx_KeyUp);
+            // 
+            // Dwell_time
+            // 
+            this.Dwell_time.AcceptsReturn = true;
+            this.Dwell_time.Location = new System.Drawing.Point(267, 165);
+            this.Dwell_time.Name = "Dwell_time";
+            this.Dwell_time.ReadOnly = true;
+            this.Dwell_time.Size = new System.Drawing.Size(196, 22);
+            this.Dwell_time.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(509, 469);
+            this.Controls.Add(this.Dwell_time);
+            this.Controls.Add(this.InBx);
+            this.Controls.Add(this.Submit);
+            this.Name = "Form1";
+            this.Text = "KBoard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.TextBox InBx;
+        private System.Windows.Forms.TextBox Dwell_time;
     }
 }
 
--- KBoard/Form1.cs
+++ KBoard/Form1.cs
@@ -7,14 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
 
 namespace KBoard
 {
     public partial class Form1 : Form
     {
+        int[] a = new int[123];
+        int b;
+        Stopwatch sw = new Stopwatch();
+        Stopwatch s2 = new Stopwatch();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InBx_KeyDown(object sender, KeyEventArgs e)
+        {
+            sw.Start();
+        }
+
+        private void InBx_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            b = (int)e.KeyChar;
+        }
+
+        private void InBx_KeyUp(object sender, KeyEventArgs e)
+        {
+            sw.Stop();
+            a[b] = (int)sw.ElapsedMilliseconds;
+            Dwell_time.Text = a[b].ToString();
+            sw.Reset();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            TextWriter tsw = new StreamWriter("kmb.csv", true);
+
+            for (int i = 97; i < 123; i++)
+            {
+                tsw.Write(a[i]);
+                tsw.Write(",");
+            }
+            tsw.Write("\n");
+
+            //Close the file.
+            tsw.Close();
+            this.Close();
+
         }
     }
 }
