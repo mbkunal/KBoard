c957f1c963cead96cadfc1f1e34dd08d5941a836
mbkunal <mb.kunal@gmail.com>
2018-10-02 21:13:10 +0530

    change 2
    i) labels added.
    ii) Check Box added
    iii) option to reopen after exit.
    iv)Option to view saved data.

--- KBoard/Form1.Designer.cs
+++ KBoard/Form1.Designer.cs
@@ -31,11 +31,18 @@
             this.Submit = new System.Windows.Forms.Button();
             this.InBx = new System.Windows.Forms.TextBox();
             this.Dwell_time = new System.Windows.Forms.TextBox();
+            this.Heading = new System.Windows.Forms.Label();
+            this.SubH = new System.Windows.Forms.Label();
+            this.PreLetter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DoOpenExcel = new System.Windows.Forms.CheckBox();
+            this.ReOpen = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // Submit
             // 
-            this.Submit.Location = new System.Drawing.Point(155, 331);
+            this.Submit.Location = new System.Drawing.Point(146, 289);
             this.Submit.Name = "Submit";
             this.Submit.Size = new System.Drawing.Size(200, 36);
             this.Submit.TabIndex = 0;
@@ -56,17 +63,97 @@
             // Dwell_time
             // 
             this.Dwell_time.AcceptsReturn = true;
-            this.Dwell_time.Location = new System.Drawing.Point(267, 165);
+            this.Dwell_time.Location = new System.Drawing.Point(271, 168);
             this.Dwell_time.Name = "Dwell_time";
             this.Dwell_time.ReadOnly = true;
             this.Dwell_time.Size = new System.Drawing.Size(196, 22);
             this.Dwell_time.TabIndex = 2;
+            // 
+            // Heading
+            // 
+            this.Heading.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Heading.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Heading.Location = new System.Drawing.Point(-9, 9);
+            this.Heading.Name = "Heading";
+            this.Heading.Size = new System.Drawing.Size(527, 64);
+            this.Heading.TabIndex = 3;
+            this.Heading.Text = "KBoard";
+            this.Heading.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // SubH
+            // 
+            this.SubH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubH.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.SubH.Location = new System.Drawing.Point(3, 73);
+            this.SubH.Name = "SubH";
+            this.SubH.Size = new System.Drawing.Size(504, 25);
+            this.SubH.TabIndex = 4;
+            this.SubH.Text = "Key to your security";
+            this.SubH.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // PreLetter
+            // 
+            this.PreLetter.Location = new System.Drawing.Point(178, 168);
+            this.PreLetter.Name = "PreLetter";
+            this.PreLetter.ReadOnly = true;
+            this.PreLetter.Size = new System.Drawing.Size(59, 22);
+            this.PreLetter.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Dwell time of  ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(243, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "is";
+            // 
+            // DoOpenExcel
+            // 
+            this.DoOpenExcel.AutoSize = true;
+            this.DoOpenExcel.Checked = true;
+            this.DoOpenExcel.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.DoOpenExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DoOpenExcel.Location = new System.Drawing.Point(26, 363);
+            this.DoOpenExcel.Name = "DoOpenExcel";
+            this.DoOpenExcel.Size = new System.Drawing.Size(277, 24);
+            this.DoOpenExcel.TabIndex = 8;
+            this.DoOpenExcel.Text = "View Data Captured after Submit";
+            this.DoOpenExcel.UseVisualStyleBackColor = true;
+            // 
+            // ReOpen
+            // 
+            this.ReOpen.AutoSize = true;
+            this.ReOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReOpen.Location = new System.Drawing.Point(29, 397);
+            this.ReOpen.Name = "ReOpen";
+            this.ReOpen.Size = new System.Drawing.Size(196, 24);
+            this.ReOpen.TabIndex = 9;
+            this.ReOpen.Text = "Re-Open After Submit";
+            this.ReOpen.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(509, 469);
+            this.Controls.Add(this.ReOpen);
+            this.Controls.Add(this.DoOpenExcel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PreLetter);
+            this.Controls.Add(this.SubH);
+            this.Controls.Add(this.Heading);
             this.Controls.Add(this.Dwell_time);
             this.Controls.Add(this.InBx);
             this.Controls.Add(this.Submit);
@@ -83,6 +170,13 @@
         private System.Windows.Forms.Button Submit;
         private System.Windows.Forms.TextBox InBx;
         private System.Windows.Forms.TextBox Dwell_time;
+        private System.Windows.Forms.Label Heading;
+        private System.Windows.Forms.Label SubH;
+        private System.Windows.Forms.TextBox PreLetter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox DoOpenExcel;
+        private System.Windows.Forms.CheckBox ReOpen;
     }
 }
 
--- KBoard/Form1.cs
+++ KBoard/Form1.cs
@@ -14,9 +14,11 @@ namespace KBoard
 {
     public partial class Form1 : Form
     {
+        private const string MyProg = @"KBoard.exe";
         int[] a = new int[123];
         int[,] seek = new int[26, 26];
         int b;
+        char pr;
         int prekey;
         Stopwatch dw = new Stopwatch();
         Stopwatch sek = new Stopwatch();
@@ -42,6 +44,7 @@ namespace KBoard
             try
             {
                 b = (int)e.KeyChar;
+                pr = e.KeyChar;
                 seek[prekey, b - 97] = (int)sek.ElapsedMilliseconds;
             }
             catch(Exception)
@@ -55,6 +58,7 @@ namespace KBoard
             dw.Stop();
             a[b] = (int)dw.ElapsedMilliseconds;
             Dwell_time.Text = a[b].ToString();
+            PreLetter.Text = pr.ToString();
             dw.Reset();
             prekey = b - 97;
             sek.Start();
@@ -63,7 +67,7 @@ namespace KBoard
         private void Submit_Click(object sender, EventArgs e)
         {
             TextWriter dwe = new StreamWriter("kmb_dwell.csv", true);
-            TextWriter sk = new StreamWriter("kmbseek.csv", true);
+            //TextWriter sk = new StreamWriter("kmbseek.csv", true);
             for (int i = 97; i < 123; i++)
             {
                 dwe.Write(a[i]);
@@ -90,10 +94,20 @@ namespace KBoard
             }
             dwe.Write("\n");
             //Close the file.
-            sk.Close();
+            //sk.Close();
             dwe.Close();
+            if (DoOpenExcel.Checked == true)
+            {
+                System.Diagnostics.Process.Start(@"kmb_dwell.csv");
+            }
+            if(ReOpen.Checked == true)
+            {
+                Process.Start(MyProg);
+            }
             this.Close();
 
         }
+
+        
     }
 }
